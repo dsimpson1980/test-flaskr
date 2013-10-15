dcb9067b48a53cf198d0e225c825cc336ac5f04d
David Simpson <mapdes@gmail.com>
2013-10-15 22:18:51 +1100

    database url

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 import os
 
-SQLALCHEMY_DATABASE_URI = urlparse.urlparse(os.environ['HEROKU_POSTGRESQL_CRIMSON_URL'])
+DATABASE = HEROKU_POSTGRESQL_CRIMSON_URL
+HOST = 'localhost'
+USERNAME = 'mapdes'
+PASSWORD = 'default'
+SQLALCHEMY_DATABASE_URI = 'postgres://%s:%s@%s/%s' % (USERNAME, PASSWORD, HOST, DATABASE)
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_ECHO = True
 DEBUG = True
 SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
+
 
 #pagination
 CUSTOMERS_PER_PAGE = 10
