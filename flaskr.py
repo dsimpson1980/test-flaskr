06e023ab9acd34da6587c430c2360a398788f4ba
David Simpson <mapdes@gmail.com>
2013-10-16 02:34:58 +1100

    change more refs to CustomerWithMarket

--- flaskr.py
+++ flaskr.py
@@ -196,7 +196,7 @@ def display_customer_premiums(customer_id, page=1):
 def display_customer(customer_id, page=1):
     if not session.get('logged_in'):
         abort(401)
-    customer = CustomerWithMarket.query.filter(Customer.customer_id==customer_id).one()
+    customer = CustomerWithMarket.query.filter(CustomerWithMarket.customer_id==customer_id).one()
     customer_demand = CustomerDemand.query.filter(CustomerDemand.customer_id==customer_id)
     customer_demand = customer_demand.paginate(page, DEMAND_ITEMS_PER_PAGE, False)
     return render_template('display_customer.html',
